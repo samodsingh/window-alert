d369f8fa0e953f54e8e793ccd0d4952340014657
samod singh <samod0098@gmail.com>
2021-05-23 13:25:47 +0530

    Arry and for loop

--- my.js
+++ my.js
@@ -1,4 +1,35 @@
+ //Arry and new Arra key testing 
+  // const arr = new Array(23,34,25,"orange");
 
-window.alert("Hii who are you ");
-var Name = "samod kumar"
-document.write("i am <br>" + Name);
+// console.log(arr);
+// arr[0]= "samod";
+// console.log(arr.length);
+
+// let mask = [ 22,30,40,50];
+// console.log(mask);
+// let value=mask.indexOf(40);
+// console.log(value);
+// mask.push(44);
+// console.log(mask);
+
+// Loop 
+// for (let i=0; i<100; i++)
+// {
+//     console.log(i);
+// }
+// let j=0;
+// while(j<10){
+//     console.log(j)
+//     j++;
+// }
+let k=0;
+do{
+ if(k===5){
+     k+=1;
+       continue ;   
+   }
+   console.log(k + 1);
+
+   k+=1;
+} while(k < 10);
+console.log("done")
